0b532a1ba6d5d68908c3cead15411ed5dbbaa470
BrightSitedev <josephtelecom.ng@gmail.com>
2026-01-20 12:07:18 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,24 @@
 'use strict';
-
 /**
- * navbar variables
+ * NAVBAR TOGGLE (DROPDOWN MODE)
  */
-const navOpenBtn = document.querySelector("[data-menu-open-btn]");
-const navCloseBtn = document.querySelector("[data-menu-close-btn]");
+const navToggler = document.querySelector("[data-nav-toggler]");
 const navbar = document.querySelector("[data-navbar]");
-const overlay = document.querySelector("[data-overlay]");
 
-const navElemArr = [navOpenBtn, navCloseBtn, overlay];
+// Toggle Menu
+navToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active"); // Animates the hamburger
+});
 
-for (let i = 0; i < navElemArr.length; i++) {
-  navElemArr[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
+// Close menu when clicking a link
+const navLinks = document.querySelectorAll(".navbar-link");
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navbar.classList.remove("active");
+    navToggler.classList.remove("active");
   });
-}
+});
 
 /**
  * header sticky functionality
